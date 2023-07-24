6c595a55ccee733e7d031e7c8fac25699aad180c
Aurélien Brabant <main@aurelienbrabant.fr>
2023-07-25 00:29:17 +0200

    Increment minor version and add build script

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pluginlab_admin',
-    version='0.0.2',
+    version='0.1.0',
     packages=find_packages(),
     install_requires=[
         "requests>=2.31.0,<3.0.0",
